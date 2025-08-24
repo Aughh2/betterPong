efc1cffcbe458a4d4a91f396880eec01e458224d
Anatolii <ankuzhlev@gmail.com>
2025-08-24 07:31:37 +0300

    added gamemanager autoload instead of using a state machine as such

---
+++ Scenes/game_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+###########################
+# Manages the flow of the game, tracks globals.
+
+###########################
--- Scripts/Game State Management/game_state_manager.gd
+++ Scripts/Game State Management/game_state_manager.gd
@@ -2,11 +2,13 @@ extends StateMachine
 class_name GameStateManager
 
 ###########################
-# Manages the flow of the game, tracks globals. Each state represents a mode. Use UIController singleton to change UI scenes. 
+# Each state represents a mode. Use UIController singleton to change UI scenes. 
 # Work in progress
 ###########################
+
+
 func _ready() -> void:
-	self.init(self)
+	self.init(get_parent())
 
 
 	
